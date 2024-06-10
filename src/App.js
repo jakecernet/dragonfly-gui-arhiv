c9772128c0922cf9f4ff13687e7c23b51bcc8808
lin-cadez <lin.cadez@gmail.com>
2024-06-10 23:22:09 +0200

    Adding simulator file. Keep INCOMING!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 import dashboardIcon from "./icons/dashboard.svg";
@@ -6,39 +6,50 @@ import settingsIcon from "./icons/settings.svg";
 import analysisIcon from "./icons/analysis.svg";
 
 import Dashboard from "./components/dashboard/dashboard";
+import GetData from "./Simulator.mjs";
 
 function App() {
-	const [selected, setSelected] = useState("dashboard");
+    const [selected, setSelected] = useState("dashboard");
+    const [displayData, setDisplayData] = useState(GetData());
 
-	return (
-		<div className="App">
-			<nav>
-				<ul>
-					<li onClick={() => setSelected("dashboard")}>
-						<a>
-							<img src={dashboardIcon} alt="Dashboard" />
-							Dashboard
-						</a>
-					</li>
-					<li onClick={() => setSelected("analysis")}>
-						<a>
-							<img src={analysisIcon} alt="Analysis" />
-							Analysis
-						</a>
-					</li>
-					<li onClick={() => setSelected("profile")}>
-						<a>
-							<img src={settingsIcon} alt="Settings" />
-							Settings
-						</a>
-					</li>
-				</ul>
-			</nav>
-			<div className="content">
-				{selected === "dashboard" && <Dashboard />}
-			</div>
-		</div>
-	);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const newData = GetData();
+            setDisplayData(newData);
+        }, 200);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="App">
+            <nav>
+                <ul>
+                    <li onClick={() => setSelected("dashboard")}>
+                        <a>
+                            <img src={dashboardIcon} alt="Dashboard" />
+                            Dashboard
+                        </a>
+                    </li>
+                    <li onClick={() => setSelected("analysis")}>
+                        <a>
+                            <img src={analysisIcon} alt="Analysis" />
+                            Analysis
+                        </a>
+                    </li>
+                    <li onClick={() => setSelected("profile")}>
+                        <a>
+                            <img src={settingsIcon} alt="Settings" />
+                            Settings
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div className="content">
+                {selected === "dashboard" && <Dashboard data={displayData} />}
+            </div>
+        </div>
+    );
 }
 
 export default App;
