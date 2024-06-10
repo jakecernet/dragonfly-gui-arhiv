7c02b0765cc50714da867337b876c8d3fdd23c61
Jaka Černetič <117542696+jakecernet@users.noreply.github.com>
2024-06-10 22:16:58 +0200

    velik nouga dizajna

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import "./App.css";
 import { useState } from "react";
+import "./App.css";
+
+import dashboardIcon from "./icons/dashboard.svg";
+import settingsIcon from "./icons/settings.svg";
+import analysisIcon from "./icons/analysis.svg";
 
 import Dashboard from "./components/dashboard/dashboard";
 
@@ -11,13 +15,22 @@ function App() {
 			<nav>
 				<ul>
 					<li onClick={() => setSelected("dashboard")}>
-						<a>Dashboard</a>
+						<a>
+							<img src={dashboardIcon} alt="Dashboard" />
+							Dashboard
+						</a>
+					</li>
+					<li onClick={() => setSelected("analysis")}>
+						<a>
+							<img src={analysisIcon} alt="Analysis" />
+							Analysis
+						</a>
 					</li>
 					<li onClick={() => setSelected("profile")}>
-						<a>Profile</a>
-					</li>
-					<li onClick={() => setSelected("settings")}>
-						<a>Settings</a>
+						<a>
+							<img src={settingsIcon} alt="Settings" />
+							Settings
+						</a>
 					</li>
 				</ul>
 			</nav>
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,12 +1,91 @@
 import "./dashboard.css";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const circleDisplay = ({ value, unit, maxRange, color }) => {
+	return (
+		<div className="circle">
+			<CircularProgressbar
+				value={value}
+				text={`${value} ${unit}`}
+				maxValue={maxRange}
+				styles={buildStyles({
+					textColor: "#fff",
+					pathColor: color || "rgb(43, 82, 189)",
+					trailColor: "#fff",
+					pathTransitionDuration: 0.5,
+					textSize: "14px",
+				})}
+			/>
+		</div>
+	);
+};
 
 function Dashboard() {
+	let temperature = 21;
+	let pressure = 2.4;
+	let voltage = 12.5;
+	let height = 204;
+	let vehicleStatus = "Armed";
+	let servoDeployed = "Undeployed";
+
 	return (
 		<div className="dashboard">
-			<container>
-				<h1>Vehicle overview</h1>
-				<div className="parameters"></div>
-			</container>
+			<h1>Vehicle overview</h1>
+			<div className="parameters">
+				<section className="text">
+					<div>
+						<h2>Relative height</h2>
+						<p>{height} m</p>
+					</div>
+					<div>
+						<h2>GPS height</h2>
+						<p>735 m</p>
+					</div>
+				</section>
+				<section className="main-four">
+					<div className="parameter">
+						{circleDisplay({
+							value: voltage,
+							unit: "V",
+							maxRange: 18,
+							color: voltage < 11 ? "red" : "rgb(43, 82, 189)",
+						})}
+					</div>
+					<div className="parameter">
+						{circleDisplay({
+							value: temperature,
+							unit: "°C",
+							maxRange: 50,
+							color:
+								temperature > 40 ? "red" : "rgb(43, 82, 189)",
+						})}
+					</div>
+					<div className="parameter">
+						{circleDisplay({
+							value: servoDeployed,
+							unit: "",
+							maxRange: 1,
+							color:
+								servoDeployed === "Deployed" ? "red" : "green",
+						})}
+					</div>
+					<div className="parameter">
+						{circleDisplay({
+							value: vehicleStatus,
+							unit: "",
+							maxRange: 1,
+							color: vehicleStatus === "Armed" ? "red" : "green",
+						})}
+					</div>
+				</section>
+				<section className="text">
+					<div>
+						<h2>Flight time</h2>
+						<p>06 : 34 : 12</p>
+					</div>
+				</section>
+			</div>
 		</div>
 	);
 }
