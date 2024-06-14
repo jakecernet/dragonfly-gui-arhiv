5a25f64b9409c04268f14bd4c84dbf02ff18c3b6
Jaka Černetič <117542696+jakecernet@users.noreply.github.com>
2024-06-14 19:08:00 +0200

    evo neki

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 import "./dashboard.css";
 
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
@@ -7,21 +7,13 @@ import "react-circular-progressbar/dist/styles.css";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-
-
-import settingsIcon from "../../icons/settings.svg";
 import locationMarker from "../../icons/location_marker.png";
-import setSelected from "../../App";
 
 const icon = new L.Icon({
 	iconUrl: locationMarker,
 	iconSize: [50, 50],
-	popupAnchor: [-25, -35],
 	iconAnchor: [25, 50],
 });
-
-
-
 
 const circleDisplay = ({ value, unit, maxRange, color }) => {
 	return (
@@ -44,16 +36,17 @@ const circleDisplay = ({ value, unit, maxRange, color }) => {
 };
 
 function Dashboard({ data }) {
+	const [InitialHeight, setInitialHeight] = useState("N/A");
+	const [InitialGPS, setInitialGPS] = useState("N/A");
 	let temperature = data.Temperature;
 	let PressureHeight = data.PressureHeight;
-	let voltage = (data.BatteryVoltage).toFixed(2);
-	let InitialHeight = data.InitialHeight;
+	let voltage = data.BatteryVoltage.toFixed(2);
 	let GPSHeight = data.GPSHeight;
 	let RelativeHeight = GPSHeight - InitialHeight;
 	let servoDeployed = data.ServoParachuteStatus ? "Deployed" : "Not deployed";
 	let beeperEnabled = data.BeeperStatus ? "On" : "Off";
 	let position = [data.GPSCords.latitude, data.GPSCords.longitude];
-
+	let positionShort = [data.GPSCords.latitude.toFixed(6), ", ", data.GPSCords.longitude.toFixed(6)];
 
 	const [servoStatus, setServoStatus] = useState(servoDeployed);
 	const [beeperStatus, setBeeperStatus] = useState(beeperEnabled);
@@ -68,15 +61,16 @@ function Dashboard({ data }) {
 		setBeeperStatus(beeperStatus === "On" ? "Off" : "On");
 	};
 
-	function handleSettingsClick() {
-		setSelected("settings");
-	}
-
 	function ChangeView({ center }) {
 		const map = useMap();
 		map.panTo(center);
 		return null;
 	}
+
+	const setInitial = () => {
+		setInitialHeight(GPSHeight);
+		setInitialGPS(positionShort);
+	};
 
 	return (
 		<div className="dashboard">
@@ -90,17 +84,29 @@ function Dashboard({ data }) {
 						</div>
 						<div>
 							<h2>GPS height</h2>
-							<p>{GPSHeight}</p>
+							<p>{GPSHeight} m</p>
 						</div>
 						<div>
 							<h2>Pressure height</h2>
 							<p>{PressureHeight} m</p>
 						</div>
-						<div className="changeable">
+					</div>
+					<div className="heights">
+						<div>
 							<h2>Initial height</h2>
-							<img src={settingsIcon} alt="Settings" onClick={handleSettingsClick} />
-							<p>{InitialHeight}m</p>
+							<p>{InitialHeight} m</p>
 						</div>
+						<div>
+							<h2>Initial GPS cords</h2>
+							<p>{InitialGPS}</p>
+						</div>
+					</div>
+					<div
+						className="initButton"
+						onClick={() => {
+							setInitial();
+						}}>
+						<a>Set current height and GPS as initial</a>
 					</div>
 				</section>
 				<section className="main-four">
@@ -144,11 +150,8 @@ function Dashboard({ data }) {
 							width: "85%",
 							borderRadius: "10px",
 							marginTop: "10px",
-						}}
-					>
-						<TileLayer
-							url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-						/>
+						}}>
+						<TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
 						<Marker position={position} icon={icon}></Marker>
 						<ChangeView center={position} />
 					</MapContainer>
